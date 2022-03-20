d8f270fcc7f09f17baf3ab96b5e77de7caaee21c
Loganathan0130 <101886109+Loganathan0130@users.noreply.github.com>
2022-03-20 17:07:11 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,27 +2,71 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Button1;
-    private Context context;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        context = this;
-        Button1 = findViewById(R.id.Button1);
-        Button1.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_splash_screen);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context,MainActivity2.class);
-                startActivity(intent);
+            gotoUrl("https://www.tutorialspoint.com/cprogramming/index.htm");
+            
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.geeksforgeeks.org/writing-first-c-program-hello-world-example/");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.geeksforgeeks.org/python-programming-language/");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.geeksforgeeks.org/java/");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.geeksforgeeks.org/html/");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.geeksforgeeks.org/css/");
             }
         });
     }
+
+
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
 }
+
